14589153f0b554914a019cb55e0ccff319e84f6f
Lucas Máximo Ferreira <lucas.maximo.ferreira21@gmail.com>
2020-11-14 17:44:20 -0300

    update

--- app/src/main/java/com/example/casadoacaitcc/Navegacao/MenuProdutos.java
+++ app/src/main/java/com/example/casadoacaitcc/Navegacao/MenuProdutos.java
@@ -27,7 +27,6 @@ import com.example.casadoacaitcc.Login;
 import com.example.casadoacaitcc.R;
 
 
-
 import de.hdodenhof.circleimageview.CircleImageView;
 import utils.utilsProduto;
 
@@ -35,6 +34,12 @@ public class MenuProdutos extends AppCompatActivity implements View.OnClickListe
 
     ImageButton btnAcai, btnSacole, btnGeladinho, btnSorvete, btnPicole, btnCremosinho;
     DrawerLayout drawerLayout;
+
+    private CircleImageView imgPerfil;
+    private static final int PICK_IMAGE_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -117,6 +122,52 @@ public class MenuProdutos extends AppCompatActivity implements View.OnClickListe
     public void ClickSair(View view) {
         Intent perfil = new Intent(this, Login.class);
         startActivity(perfil);
+    }
+
+    public void ClickImg(View view) {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+
+                requestPermissions(permissions, PERMISSION_CODE);
+            } else {
+                pickImageFromGallery();
+            }
+        } else {
+            pickImageFromGallery();
+        }
+    }
+
+    //////// ADICIONAR FOTO PELA GALERIA
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery();
+                }
+                else{
+                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE_CODE) {
+            imgPerfil.setImageURI(data.getData());
+        }
     }
 
     @Override
--- app/src/main/java/com/example/casadoacaitcc/Navegacao/main_nav_drawer.java
+++ app/src/main/java/com/example/casadoacaitcc/Navegacao/main_nav_drawer.java
@@ -35,62 +35,11 @@ public class main_nav_drawer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_nav_drawer);
 
-        //////// ADICIONAR FOTO PELA GALERIA
 
-        imgPerfil = (CircleImageView) findViewById(R.id.imgPerfil);
-
-        imgPerfil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-
-                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-
-                        requestPermissions(permissions, PERMISSION_CODE);
-                    } else {
-                        pickImageFromGallery();
-                    }
-                } else {
-                    pickImageFromGallery();
-                }
-            }
-        });
-        ///////////////////////
 
     }
 
 
-    //////// ADICIONAR FOTO PELA GALERIA
-    private void pickImageFromGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, PICK_IMAGE_CODE);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case PERMISSION_CODE:{
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    pickImageFromGallery();
-                }
-                else{
-                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE_CODE) {
-            imgPerfil.setImageURI(data.getData());
-        }
-    }
-//////////////////////////////////
 
 
     public void ClickMenuProdutos(View view) {
@@ -128,4 +77,51 @@ public class main_nav_drawer extends AppCompatActivity {
         Intent perfil = new Intent(this, Login.class);
         startActivity(perfil);
     }
+
+    public void ClickImg(View view) {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+
+                requestPermissions(permissions, PERMISSION_CODE);
+            } else {
+                pickImageFromGallery();
+            }
+        } else {
+            pickImageFromGallery();
+        }
+    }
+
+    //////// ADICIONAR FOTO PELA GALERIA
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery();
+                }
+                else{
+                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE_CODE) {
+            imgPerfil.setImageURI(data.getData());
+        }
+    }
+//////////////////////////////////
 }
