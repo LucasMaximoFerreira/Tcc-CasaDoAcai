e7030199d4c0571276e816a2d636b036ea19b11a
Lucas Máximo Ferreira <lucas.maximo.ferreira21@gmail.com>
2020-12-01 19:19:52 -0300

    Validacao de email e senha

--- app/src/main/java/com/example/casadoacaitcc/CadastroCliente/Cadastro1.java
+++ app/src/main/java/com/example/casadoacaitcc/CadastroCliente/Cadastro1.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextPaint;
 import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,13 +21,25 @@ import com.example.casadoacaitcc.R;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
+import java.util.regex.Pattern;
+
 import utils.utilsCadastro_cliente;
 
 public class Cadastro1 extends AppCompatActivity implements View.OnClickListener {
+    private static final Pattern SENHA_VAL =
+            Pattern.compile("^" +
+                    "(?=.*[0-9])" +
+                    "(?=.*[a-z])" +
+                    "(?=.*[A-Z])" +
+                    "(?=.*[@#$%^&+=])" +
+                    "(?=\\S+$)" +
+                    ".{6,}" +
+                    "$");
 
     EditText txtNome, txtSenha, txtEmail, txtData;
-    Button btnCad1;
+    Button btnCad1, btnVerificar;
     TextView lblFacaLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +53,9 @@ public class Cadastro1 extends AppCompatActivity implements View.OnClickListener
         lblFacaLogin = findViewById(R.id.lblFacaLogin);
         btnCad1.setOnClickListener(this);
         lblFacaLogin.setOnClickListener(this);
+        btnVerificar = findViewById(R.id.btnVerificar);
 
+        btnVerificar.setOnClickListener(this);
         txtNome.addTextChangedListener(cadastro1TextWatcher);
         txtSenha.addTextChangedListener(cadastro1TextWatcher);
         txtEmail.addTextChangedListener(cadastro1TextWatcher);
@@ -66,11 +81,9 @@ public class Cadastro1 extends AppCompatActivity implements View.OnClickListener
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             String nomeInput = txtNome.getText().toString().trim();
-            String senhaInput = txtSenha.getText().toString().trim();
-            String emailInput = txtEmail.getText().toString().trim();
             String dataInput = txtData.getText().toString().trim();
 
-            btnCad1.setEnabled(!nomeInput.isEmpty() && !senhaInput.isEmpty() && !emailInput.isEmpty() && !dataInput.isEmpty());
+            btnCad1.setEnabled(!nomeInput.isEmpty() && !dataInput.isEmpty());
 
         }
 
@@ -81,11 +94,11 @@ public class Cadastro1 extends AppCompatActivity implements View.OnClickListener
     };
 
     //FUNCAO PARA COLORIR O LABEL EM DEGRADE
-    private void setTextCorDegrade(){
+    private void setTextCorDegrade() {
         TextPaint paint = lblFacaLogin.getPaint();
         float width = paint.measureText("faça o login aqui");
 
-        Shader shader = new LinearGradient(0,0,width,lblFacaLogin.getTextSize(),
+        Shader shader = new LinearGradient(0, 0, width, lblFacaLogin.getTextSize(),
                 new int[]{
                         Color.parseColor("#9300E9"),
                         Color.parseColor("#BF0085"),
@@ -95,17 +108,54 @@ public class Cadastro1 extends AppCompatActivity implements View.OnClickListener
         lblFacaLogin.getPaint().setShader(shader);
     }
 
+    private Boolean validarSenha() {
+        String senha = txtSenha.getText().toString();
+        if (senha.isEmpty()) {
+            txtSenha.setError("campo não pode ser vazio");
+            btnCad1.setEnabled(false);
+            return false;
+        } else if (!SENHA_VAL.matcher(senha).matches()) {
+            txtSenha.setError("Senha Fraca");
+            btnCad1.setEnabled(false);
+            return false;
+        } else {
+            txtSenha.setError(null);
+            btnCad1.setEnabled(true);
+            return true;
+        }
+    }
+
+    private Boolean validarEmail() {
+        String email = txtEmail.getText().toString();
+        if (email.isEmpty()) {
+            txtEmail.setError("campo não pode ser vazio");
+            btnCad1.setEnabled(false);
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            txtEmail.setError("digite um email válido");
+            btnCad1.setEnabled(false);
+            return false;
+        } else {
+            txtEmail.setError(null);
+            btnCad1.setEnabled(true);
+            return true;
+        }
+    }
+
     @Override
     public void onClick(View v) {
 
         switch (v.getId()) {
             case R.id.btnCad1:
 
+
                 utilsCadastro_cliente.setUnome_cli(txtNome.getText().toString());
                 utilsCadastro_cliente.setUsenha_cli(txtSenha.getText().toString());
                 utilsCadastro_cliente.setUemail_cli(txtEmail.getText().toString());
+
                 utilsCadastro_cliente.setUdtnasc_cli(txtData.getText().toString());
 
+                validarSenha();
 
                 Intent telaCad2 = new Intent(this, Cadastro2.class);
                 startActivity(telaCad2);
@@ -114,6 +164,9 @@ public class Cadastro1 extends AppCompatActivity implements View.OnClickListener
             case R.id.lblFacaLogin:
                 Intent login = new Intent(this, Login.class);
                 startActivity(login);
+                break;
+            case R.id.btnVerificar:
+                validarEmail();
                 break;
         }
     }
